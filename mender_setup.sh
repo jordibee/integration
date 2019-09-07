243df03cc2d35b8bd533c722bffba0cf2219d9b8
Jordi <jordi@beethedata.com>
2019-09-07 20:01:16 +0200

    Finally make this work.

--- mender_setup.sh
+++ mender_setup.sh
@@ -37,11 +37,9 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update && sudo apt-get install -y docker-ce
-sudo usermod -a -G docker $USER
 sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -a -G docker $USER
 
 # Format and mount drive we will be using to store artifacts.
 echo "[*] Formatting and mounting artifacts drive..."
@@ -76,5 +74,5 @@ CERT_API_CN=${1} CERT_STORAGE_CN=${2} ../keygen
 
 # Start the server.
 echo "[*] Starting server..."
-./run up -d
+sudo ./run up -d
 popd &> /dev/null
